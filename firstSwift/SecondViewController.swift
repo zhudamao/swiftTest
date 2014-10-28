cd75bfae8bbec326cda311854f9b747189856e39
zhudamao <zhudamao5201314@163.com>
2014-10-28 17:32:53 +0800

    gcd http
    use gcd to load http data

--- firstSwift/SecondViewController.swift
+++ firstSwift/SecondViewController.swift
@@ -9,18 +9,46 @@
 import UIKit
 
 class SecondViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    let urlApi = "http://qingbin.sinaapp.com/api/lists?ntype=%E5%9B%BE%E7%89%87&pageNo=1&pagePer=10&list.htm"
+    var dataArry = []
     
     @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = NSURL(string: "http://www.baidu.com")
-        let request = NSURLRequest(URL: url!, cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy, timeoutInterval: 10)
-        var respone:NSURLResponse?
-        let data = NSURLConnection.sendSynchronousRequest(request, returningResponse: &respone, error: nil)
-        let str = NSString(data: data!, encoding: NSUTF8StringEncoding)
         
-        println(str)
+        let refreshContrl = UIRefreshControl()
+        refreshContrl.attributedTitle = NSAttributedString(string: "下拉刷新")
+        refreshContrl.addTarget(self, action: "loadData", forControlEvents: UIControlEvents.ValueChanged)
+        
+        
+//        let url = NSURL(string: self.urlApi)
+//        let request = NSURLRequest(URL: url!, cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy, timeoutInterval: 10)
+//        var respone:NSURLResponse?
+//        let data = NSURLConnection.sendSynchronousRequest(request, returningResponse: &respone, error: nil)
+//        let str = NSString(data: data!, encoding: NSUTF8StringEncoding)
+        
+        self.loadData()
     }
+    
+    func loadData(){
+        let url = NSURL(string: self.urlApi)
+        let request = NSURLRequest(URL: url!, cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy, timeoutInterval: 10)
+         var respone:NSURLResponse?
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            var respone:NSURLResponse?
+            var error:NSError?
+            let data = NSURLConnection.sendSynchronousRequest(request, returningResponse: &respone, error: &error)
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                let json = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+                self.dataArry = json["item"] as NSArray
+                self.tableView.reloadData()
+            })
+            
+        })
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -33,19 +61,44 @@ class SecondViewController: UIViewController,UITableViewDelegate,UITableViewData
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let reuseIdentifier = "cell"
-        var cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier)
+        var cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier) as? UITableViewCell
         if cell == nil{
-            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: reuseIdentifier)
-            cell?.textLabel.text = "row is \(indexPath.row)"
+            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: reuseIdentifier)
         }
         
-        return cell as UITableViewCell
+        let object = self.dataArry[indexPath.row] as NSDictionary
+        
+        cell?.textLabel.text = object["title"] as NSString
+        cell?.detailTextLabel?.text = object["id"] as NSString
+
+        let urlHead = object["thumb"] as NSString
+        let headUrl = NSURL(string: urlHead)
+        let requet = NSURLRequest(URL: headUrl!, cachePolicy: NSURLRequestCachePolicy.UseProtocolCachePolicy, timeoutInterval: 10)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            var error:NSError?
+            var respone:NSURLResponse?
+            let data = NSURLConnection.sendSynchronousRequest(requet, returningResponse: &respone, error: &error)
+            if data == nil {
+                return
+            }
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+
+                var image = UIImage(data: data!)
+                //image = image?.resizableImageWithCapInsets(UIEdgeInsets(top: 2, left: 2, bottom: 2, right: 2))
+                cell?.imageView.image = image
+            })
+        })
+        
+        return cell!
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10;
+        return self.dataArry.count;
     }
     
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 60.0
+    }
     /*
     // MARK: - Navigation
 
